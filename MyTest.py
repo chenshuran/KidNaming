3106d0101c4cf1345137e9bb16e0de80e0012a13
Ryan <matrixran@gmail.com>
2021-03-27 21:55:39 +0800

    some no used file

--- MyTest.py
+++ MyTest.py
@@ -16,6 +16,37 @@ charstring_ori="本,尕,甘,功,古,瓜,宄,卉,加,甲,叫,句,巨,卡,可,叩,
 charstring_tmp=Mylib.charStrtolist(charstring_ori)
 chinchar5mu= Mylib.printChineseCharlist(charstring_tmp,list(),5,"木","")
 
+# 6画木字
+charstring_ori="朳,朵,尬,各,共,乩,吉,伎,奸,幵,囝,件,交,臼,伉,考,朽,旭,匡,夼,企,犰,曲,戎,仰,吁,聿,朱,竹"
+charstring_tmp=Mylib.charStrtolist(charstring_ori)
+chinchar6mu= Mylib.printChineseCharlist(charstring_tmp,list(),6,"木","")
+
+# 7画木字
+charstring_ori="杓,材,岑,杈,床,村,杜,呃,伽,改,杆,杠,告,更,攻,估,谷,庋,呙,囯,旱,何,吼,肓,妓,忌,夹,见,角,疖,劫,妗,究,局,姖,君,佧,扛,囥,壳,克,扣,困,伲,你,杞,扦,羌,劬,却,杉,我,吴,吾,扤,杌,匣,吓,杏,言,吟,杖"
+charstring_tmp=Mylib.charStrtolist(charstring_ori)
+chinchar6mu= Mylib.printChineseCharlist(charstring_tmp,list(),7,"木","")
+
+# 8画木字
+charstring_ori="昂,枊,板,杯,杵,东,妸,扼,枋,斧,秆,杲,疙,供,咕,姑,孤,固,呱,乖,官,果,杭,忽,昏,肌,亟,佶,技,季,佳,肩,艽,佼,届,卺,京,纠,赳,玖,疚,居,咀,具,卷,咔,咖,抗,肯,空,枕,枝,快,狂,林,枚,杪,艿,呢,杻,杷,枇,其,奇,歧,穹,虬,屈,券,枘,松,枉,卧,析,呷,欣,芎,厓,兖,杳,宜,竺,杼"
+charstring_tmp=Mylib.charStrtolist(charstring_ori)
+chinchar6mu= Mylib.printChineseCharlist(charstring_tmp,list(),8,"木","")
+
+# 9画木字
+charstring_ori="柲,柄,柴,柢,芏,俄,枹,尜,肝,柑,竿,疳,肛,缸,纥,革,虼,哏,狗,枸,牯,故,冠,咣,皈,轨,癸,柜,哄,訇,虹,芨,咭,级,急,纪,既,枷,叚,架,建,牮,姜,姣,皆,界,疥,蚧,矜,劲,扃,九,韭,拘,狙,拒,军,看,柯,科,咳,客,芝,哐,柃,柳,咯,芒,怩,昵,拈,柈,芃,枰,祈,芑,契,恰,芊,俏,俅,酋,畎,芍,柿,柁,柝,芄,玩,侠,狎,柙,相,枵,彦,奕,弈,疫,羿,胤,柚,禺,竽,芋,栅,柘,枳,柱,斫,柞"
+charstring_tmp=Mylib.charStrtolist(charstring_ori)
+chinchar9mu= Mylib.printChineseCharlist(charstring_tmp,list(),9,"木","")
+
+# 10画木字
+charstring_ori="桉 芭 笆 柏 栢 梆 苄 栟 屙 婀 苊 芳 芬 粉 芙 酐 绀 羔 高 哥 格 鬲 哿 根 耕 哽 肱 恭 蚣 躬 拱 贡 股 骨 罟 挂 倌 桄 鬼 桂 衮 核 桁 讧 笏 花 桓 恢 唧 姬 屐 笄 笈 脊 记 珈 家 痂 恝 兼 豇 狡 讦 拮 桀 芥 衿 肼 弪 径 柩 桕 疽 桔 俱 倨 娟 倦 桊 隽 拷 栲 珂 疴 恪 倥 恐 芤 栩 祗 哭 库 框 括 栝 栳 栗 匿 臬 耙 芘 栖 芪 耆 岂 起 气 虔 肷 芡 衾 芩 芹 祛 拳 缺 桑 芟 秫 栓 凇 笋 桃 桐 砼 桅 芴 奚 哮 校 芯 桠 芽 芫 唁 苡 倚 痈 邕 娱 圄 峪 原 纭 芸 笊 芷 桎 株 桌"
+charstring_tmp=Mylib.charStrtolist(charstring_ori)
+chinchar10mu= Mylib.printChineseCharlist(charstring_tmp,list(),10,"木","")
+
+# 11画木字
+charstring_ori="茇,苞,苯,笨,苾,梐,彬,梣,茬,趁,笞,茌,崔,笪,笛,兜,轭,梵,苻,桴,符,苷,敢,舸,梗,珙,苟,笱,蛄,梏,牿,胍,规,匦,国,悍,捍,偈,寄,笳,袈,戛,蛱,胛,徦,假,坚,枧,趼,健,皎,教,秸,婕,堇,近,婧,竟,救,苴,趄,苣,捐,眷,桷,捃,胩,康,苛,氪,啃,寇,许,苦,眶,悝,盔,悃,捆,梱,婪,笠,啉,苓,茅,茆,茂,梅,苠,茉,苜,旎,偶,苤,笸,启,弃,乾,悄,茄,卿,顷,筇,蚯,区,蛆,朐,娶,悛,圈,痊,苒,若,啬,苫,梢,苕,笙,倏,梳,术,笥,桫,梭,苔,梯,笤,梃,桶,偓,梧,悟,晤,狭,厢,枭,偕,械,研,厣,眼,悒,挹,翊,茚,英,唷,圉,庾,苑,笮,苎,棁,茁,梓"
+charstring_tmp=Mylib.charStrtolist(charstring_ori)
+chinchar11mu= Mylib.printChineseCharlist(charstring_tmp,list(),11,"木","")
+
+
 # 5画火字
 charstring_ori="丙,代,旦,叨,氐,叮,冬,叻,立,尥,令,另,奶,尼,奴,冉,他,它,田,仝,仗,召,只,左"
 Huo_5new=Mylib.charStrtolist(charstring_ori)
